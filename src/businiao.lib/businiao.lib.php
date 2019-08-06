658078d25253244f4ff81d2ee219b3fe52c94f62
goodyes-hp <wanjun8888@gmail.com>
2019-08-06 22:04:29 +0800

    notice

--- src/businiao.lib/businiao.lib.php
+++ src/businiao.lib/businiao.lib.php
@@ -3,6 +3,6 @@ include_once 'businiao.defined.php';
 include_once 'businiao.curlLib.php';
 include_once 'businiao.api.php';
 /**github
-git tag -a v1.1.1 -m 'v1.1.1'
+git tag -a v1.1.2 -m 'v1.1.2'
 git push origin --tags
  * */
